8a6276e852b22fefd817e6b53f4d3d6762e9762b
breadwall <108824635+breadwall@users.noreply.github.com>
2022-07-06 12:49:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to Breadwall the creaters of Luna Adventure!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Contact breadwall649@gmail.com for questions or concerns.
         </p>
       </main>
 
